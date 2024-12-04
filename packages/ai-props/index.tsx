4c4566b410472929469c94c2981c46f5c5e9ad6b
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2024-12-03 19:05:58 -0600

    Added args

--- packages/ai-props/index.tsx
+++ packages/ai-props/index.tsx
@@ -1,5 +1,6 @@
 import { generateObject } from 'ai'
 import { z, ZodSchema } from 'zod'
+import { dump } from 'js-yaml'
 import React from 'react'
 // import { Suspense } from 'react'
 
@@ -9,10 +10,12 @@ type AISchema = {
 
 export type AIProps<T> = {
   children: React.ReactElement<T>
+  args: any
   schema: ZodSchema<T> 
 } & Parameters<typeof generateObject>[0]
 
-export const AI = async <T,>({ children,  ...config }: AIProps<T>) => {
+export const AI = async <T,>({ children, args,  ...config }: AIProps<T>) => {
+  config.prompt = config.prompt ? `${config.prompt}\n\n${dump(args)}` : dump(args)
   const { object } = await generateObject(config)
   return React.isValidElement(children) 
     ? React.cloneElement(children, object as Partial<T>)
