dec6128171b79d8b5a7ff56b12076c1062c01d8a
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2024-12-05 00:48:00 -0600

    Added evals

---
+++ packages/ai-functions/ai/test.eval.ts
@@ -0,0 +1,15 @@
+import { createScorer, evalite } from 'evalite';
+
+const containsParis = createScorer<string, string>('Contains Paris', ({ output }) => {
+  return output.includes('Paris') ? 1 : 0
+});
+
+evalite('My Eval', {
+  data: async () => {
+    return [{ input: 'Hello', output: 'Hello World!' }]
+  },
+  task: async (input: string) => {
+    return input + ' World!'
+  },
+  scorers: [containsParis],
+})
