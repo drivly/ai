188261611f9f6d458f66231e431d278f4148ec6a
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-03-19 08:34:41 -0500

    feat(api): dynamic domain links

--- dash/app/(apis)/api/route.ts
+++ dash/app/(apis)/api/route.ts
@@ -1,10 +1,33 @@
 import { API } from '@/api.config'
 
-export const GET = API(async (request, { db, user, origin, url }) => {
+export const GET = API(async (request, { db, user, origin, url, domain }) => {
   // Using the new db interface for more concise syntax
   // const functions = await db.functions.find()
 
-  return {
+  return domain === 'apis.do' ? {
+    "featured": {
+      "Functions - Typesafe Results without Complexity": "https://functions.do",
+      "Workflows - Reliably Execute Business Processes": "https://workflows.do",
+      "Agents - Deploy & Manage Autonomous Digital Workers": "https://agents.do"
+    },
+    "things": {
+      "Nouns - People, Places, Things, and Ideas": "https://nouns.do",
+      "Verbs - The Actions Performed to and by Nouns": "https://verbs.do"
+    },
+    "events": {
+      "Triggers - Initiate workflows based on events": "https://triggers.do",
+      "Searches - Query and retrieve data": "https://searches.do",
+      "Actions - Perform tasks within workflows": "https://actions.do"
+    },
+    "core": {
+      "LLM - Intelligent AI Gateway": "https://llm.do",
+      "Evals - Evaluate Functions, Workflows, and Agents": "https://evals.do",
+      "Analytics - Economically Validate Workflows": "https://analytics.do",
+      "Experiments - Economically Validate Workflows": "https://experiments.do",
+      "Database - AI Native Data Access (Search + CRUD)": "https://database.do",
+      "Integrations - Connect External APIs and Systems": "https://integrations.do"
+    }
+  } : {
     featured: {
       'Functions - Typesafe Results without Complexity': origin + '/functions',
       'Workflows - Reliably Execute Business Processes': origin + '/workflows',
