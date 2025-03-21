64536265ce1dbeb13d6ba884a911b9e21e39c39b
AggressivelyMeows <25571984+AggressivelyMeows@users.noreply.github.com>
2025-03-21 14:48:33 +0000

    chore: update .gitignore and package.json for environment variables and package manager version

---
+++ app/(apis)/api/models/route.ts
@@ -0,0 +1,20 @@
+import { API } from '@/api.config'
+import { getModel } from '@/pkgs/ai-models/src'
+
+export const GET = API(async (request, { db, user, origin, url, domain, params }) => {
+  // Using the new db interface for more concise syntax
+  // const functions = await db.functions.find()
+
+  const qs = new URLSearchParams(request.url.split('?')[1])
+  const model = qs.get('model')
+
+  if (model) {
+    return {
+      resolvedModel: getModel(model)
+    }
+  }
+
+  return {
+    models: []
+  }
+})
