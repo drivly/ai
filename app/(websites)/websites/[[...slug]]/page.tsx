4db506d67ecf5d7d9eaf5241224b1a685c3a0717
Chris Risner <chris@driv.ly>
2025-03-21 17:16:50 -0500

    feat(websites): added typesafe array of possible websites for catchall page, also notFound if not supported

--- app/(websites)/websites/[[...slug]]/page.tsx
+++ app/(websites)/websites/[[...slug]]/page.tsx
@@ -1,8 +1,18 @@
+import type { Website } from '@/website.config'
+import { websiteKeys } from '@/website.config'
+import { notFound } from 'next/navigation'
 import '../styles.css'
 
 // need to be able to render the specific website from the slug and throw not found if the slug is not found
 export default async function HomePage({ params }: { params: Promise<{ slug: string[] }> }) {
   const { slug } = await params
+
+  const site = slug[0] as Website
+
+  if (site && !websiteKeys.includes(site)) {
+    return notFound()
+  }
+
   return (
     <div className='home'>
       <h1>{slug[0]}</h1>
--- website.config.ts
+++ website.config.ts
@@ -1,7 +1,9 @@
+import { objectKeys } from './lib/utils'
+
 export const aliases = {
   'databases.do': 'database.do',
   'llms.do': 'llm.do',
-}
+} as const
 
 export const websites = {
   'functions.do': '',
@@ -9,4 +11,8 @@ export const websites = {
   'agents.do': 'agents',
   'llm.do': 'llm',
   'dotdo.co': '.do | Economically Valuable Work',
-}
+} as const
+
+export type Website = keyof typeof websites | (typeof aliases)[keyof typeof aliases] | keyof typeof aliases
+
+export const websiteKeys = [...objectKeys(websites), ...objectKeys(aliases), ...Object.values(aliases)]
