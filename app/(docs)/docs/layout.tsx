cccd7d8ef06cd89b21d23dc3d75fe0928207973c
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-03-22 09:40:26 -0500

    docs: updated content and links

--- app/(docs)/docs/layout.tsx
+++ app/(docs)/docs/layout.tsx
@@ -10,14 +10,15 @@ export const metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
 
-const banner = <Banner storageKey='some-key'>Nextra 4.0 is released 🎉</Banner>
+const banner = <Banner storageKey='some-key'>Functions.do is released 🎉</Banner>
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
+    logo={<b>Workflows.do</b>}
+    chatLink='https://discord.gg/a87bSRvJkx'
     // ... Your additional navbar options
   />
 )
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
+const footer = <Footer>MIT {new Date().getFullYear()} © Workflows.do</Footer>
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -39,7 +40,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase='https://github.com/drivly/ai/tree/main/content'
+          docsRepositoryBase='https://github.com/drivly/ai/tree/main'
           footer={footer}
           // ... Your additional layout options
         >
