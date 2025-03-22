baca01ccfec3bec5afa31ccde8f83a4c124fae8b
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-03-22 04:38:15 -0500

    feat(website): added vercel analytics
    closes #73

--- app/(docs)/docs/layout.tsx
+++ app/(docs)/docs/layout.tsx
@@ -2,6 +2,7 @@ import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Banner, Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   // Define your metadata here
@@ -43,6 +44,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         >
           {children}
         </Layout>
+        <Analytics />
       </body>
     </html>
   )
--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -8,6 +8,7 @@ import React from 'react'
 
 import { importMap } from './admin/importMap.js'
 import './custom.scss'
+import { Analytics } from "@vercel/analytics/react"
 
 type Args = {
   children: React.ReactNode
@@ -25,6 +26,7 @@ const serverFunction: ServerFunctionClient = async function (args) {
 const Layout = ({ children }: Args) => (
   <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
     {children}
+    <Analytics />
   </RootLayout>
 )
 
