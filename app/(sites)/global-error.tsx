af2ecd32ebed4e9f36bc1505da44f07ca21aa80a
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-03-23 10:05:32 -0500

    fix(admin): added error handler

---
+++ app/(sites)/global-error.tsx
@@ -0,0 +1,22 @@
+'use client' // Error boundaries must be Client Components
+ 
+// TODO: figure out if we can save errors to db via a server action
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    // global-error must include html and body tags
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <pre><code>{error.message + '\n\n' + (error.stack || '')}</code></pre>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  )
+}
