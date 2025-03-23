6a3acd0a14d2a8fcd20b3cf22f482e76403a1873
Samuel Lippert <lippertz@gmail.com>
2025-03-22 21:51:20 -0500

    feat(llm.do): add getModel

--- workers/llm.do/src/endpoints/chatCompletionCreate.ts
+++ workers/llm.do/src/endpoints/chatCompletionCreate.ts
@@ -1,7 +1,7 @@
+import { getModel } from 'ai-models'
 import { OpenAPIRoute } from 'chanfana'
 import { fetchFromProvider } from 'providers/openRouter'
 import { AuthHeader, ChatCompletionRequest, ChatCompletionResponse } from '../types'
-import { parse } from '@ai-primitives/ai-models'
 
 export class ChatCompletionCreate extends OpenAPIRoute {
   schema = {
@@ -34,8 +34,8 @@ export class ChatCompletionCreate extends OpenAPIRoute {
     const request = await this.getValidatedData<typeof this.schema>()
 
     // Model router
-    const { model, author, provider, capabilities } = parse(request.body.model || '')
-    request.body.model = (provider && author ? `${provider}/${author}/${model}` : `${author ? author + '/' : ''}${model}`) + (capabilities ? `:${capabilities.join(',')}` : '')
+    const modelResult = getModel(request.body.model || '')
+    request.body.model = modelResult?.slug
 
     // Pass request to OpenRouter
     return await fetchFromProvider(request, 'POST', '/chat/completions')
