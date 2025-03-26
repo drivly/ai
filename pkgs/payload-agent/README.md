317823e8b96622ad6543cb4c3f2866ec54449f47
Chris Risner <chris@driv.ly>
2025-03-26 14:30:41 -0500

    feat(payload-agent): inital package

--- pkgs/payload-agent/README.md
+++ pkgs/payload-agent/README.md
@@ -1,1 +1,27 @@
-# TBD
+# ChatBot
+
+Can be used as a panel, modal or resizable.
+
+## Panel
+
+```tsx
+<ChatBot logo="/DrivlyLogo.svg" type="panel" withOverlay withOutsideClick />
+```
+
+## Modal
+
+```tsx
+<ChatBot logo="/DrivlyLogo.svg" type="modal" withOverlay withOutsideClick />
+```
+
+## Resizable
+
+needs to wrap your entire app in order to work.
+
+```tsx
+<ChatBot logo="/DrivlyLogo.svg" type="resizable" direction="horizontal">
+  <div>
+    <h1>Main Application</h1>
+  </div>
+</ChatBot>
+```
---
+++ pkgs/payload-agent/src/components/chat/client-container.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { cn } from '@drivly/ui/lib'
+import { MessageSquare } from 'lucide-react'
+import { Fragment } from 'react'
+import { Wrapper } from '../../lib'
+import type { PayloadAgentAuthResult } from '../../types/auth'
+import type { ClientContainerProps } from '../../types/chat'
+import * as Container from './chat-container'
+
+const ClientContainer: React.FC<ClientContainerProps> = ({
+  aiAvatar,
+  children,
+  defaultMessage,
+  chatOptions,
+  logo,
+  title,
+  type,
+  direction,
+  withOverlay,
+  withOutsideClick,
+  suggestions,
+  initialAuthResult,
+}) => {
+  return (
+    <Fragment>
+      {type !== 'resizable' && children}
+      <Wrapper
+        as={type === 'modal' ? Container.Root : type === 'panel' ? Container.PanelRoot : Container.ResizableRoot}
+        defaultMessage={defaultMessage}
+        logo={logo}
+        title={title}
+        initialAuthResult={initialAuthResult}
+        className={chatOptions?.rootStyle}
+        layoutChildren={type === 'resizable' && children}
+        direction={type === 'resizable' ? direction : undefined}
+        suggestions={suggestions}
+        aiAvatar={aiAvatar}>
+        <Container.Trigger
+          className={cn(chatOptions?.triggerStyle, {
+            'right-[32px] bottom-[16px]': type === 'resizable',
+          })}>
+          <MessageSquare size={18} />
+        </Container.Trigger>
+        <Wrapper
+          as={type === 'modal' ? Container.Modal : type === 'panel' ? Container.Panel : Container.Resizable}
+          withOverlay={withOverlay}
+          withOutsideClick={withOutsideClick}
+          className={chatOptions?.containerStyle}>
+          <Container.Header
+            className={chatOptions?.headerStyle}
+            buttonStyle={chatOptions?.headerButtonStyle}
+            logoStyle={chatOptions?.headerLogoStyle}
+            titleStyle={chatOptions?.headerTitleStyle}
+          />
+          <Container.Content />
+        </Wrapper>
+      </Wrapper>
+    </Fragment>
+  )
+}
+
+export default ClientContainer
