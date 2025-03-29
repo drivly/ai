e89f4557103251c95162e7382bd082dd0335328b
Samuel Lippert <lippertz@gmail.com>
2025-03-29 04:47:14 -0500

    fix(ai-models): comma parse

--- api-tests/e2e/openai.test.ts
+++ api-tests/e2e/openai.test.ts
@@ -1,32 +1,56 @@
 import { expect, test, describe } from 'vitest'
 import OpenAI from 'openai'
+import type { ResponseInput } from 'openai/resources/responses/responses.mjs'
+import fs from 'fs'
 
 // Explicitly call out llm.do and OpenRouter key usage
 const client = new OpenAI({
   apiKey: process.env.OPENRROUTER_API_KEY,
-  baseURL: 'https://llm.do/api/v1',
-  // baseURL: 'http://127.0.0.1:8787/api/v1',
+  // baseURL: 'https://llm.do/api/v1',
+  baseURL: 'http://127.0.0.1:8787/api/v1',
 })
 
 describe('OpenAI SDK', () => {
-  test('can create a response', async () => {
-    const response = await client.responses.create({
-      input: 'Hello, world!',
-      model: 'gpt-4o',
+  test('can handle PDF input', async () => {
+    const input: ResponseInput = [
+      {
+        role: 'user',
+        content: [
+          // {
+          //   type: 'input_file',
+          //   filename: 'ORMwhitepaper.pdf',
+          //   file_data: 'data:application/pdf;base64,' + fs.readFileSync('./e2e/ORMwhitepaper.pdf', 'base64'),
+          // },
+          {
+            type: 'input_text',
+            text: 'What are the steps of CSDP?',
+          },
+        ],
+      },
+    ]
+
+    let response = await client.responses.create({
+      model: 'gemini-2.0-flash',
+      input,
     })
     console.log(response)
     expect(response).toBeDefined()
     expect(response.id).toBeDefined()
-  })
 
-  test.fails('can create an image', async () => {
-    const response = await client.images.generate({
-      prompt: 'A beautiful sunset over a calm ocean',
-      n: 1,
-      size: '1024x1024',
+    response = await client.responses.create({
+      model: 'gpt-4o',
+      input,
     })
     console.log(response)
     expect(response).toBeDefined()
-    expect(response.data[0].url).toBeDefined()
-  })
+    expect(response.id).toBeDefined()
+
+    response = await client.responses.create({
+      model: 'claude-3-7-sonnet',
+      input,
+    })
+    console.log(response)
+    expect(response).toBeDefined()
+    expect(response.id).toBeDefined()
+  }, 60000)
 })
