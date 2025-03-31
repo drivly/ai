1266b9529a37888bc2b99cc783abb4ee7f1d5b6b
Chris Risner <chris@driv.ly>
2025-03-31 12:05:25 -0500

    build importmap

--- app/(payload)/admin/importMap.js
+++ app/(payload)/admin/importMap.js
@@ -1,9 +1,9 @@
 import { WorkflowView as WorkflowView_f4d5503956e2d624777a2e6c0bc05047 } from 'payload-kanban-board/dist/exports/client.js'
-import { default as default_e7deab0ce526bfd20c5617b32b88ad99 } from '@/components/icon.tsx'
-import { default as default_98b9c1e527fb6c856d7dd54df5e3faa4 } from '@/components/logo.tsx'
+import { default as default_f8022cf35b3d492829ec1a405bd134e9 } from '@/components/icon'
+import { default as default_7a89b5675912f3f4f82dd41bbb03d52e } from '@/components/logo'
 
 export const importMap = {
   "payload-kanban-board/dist/exports/client.js#WorkflowView": WorkflowView_f4d5503956e2d624777a2e6c0bc05047,
-  "@/components/icon.tsx#default": default_e7deab0ce526bfd20c5617b32b88ad99,
-  "@/components/logo.tsx#default": default_98b9c1e527fb6c856d7dd54df5e3faa4
+  "@/components/icon#default": default_f8022cf35b3d492829ec1a405bd134e9,
+  "@/components/logo#default": default_7a89b5675912f3f4f82dd41bbb03d52e
 }
