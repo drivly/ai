82e49aac55c612ad62f3a0c689260bcb9c3af88d
Chris Risner <chris@driv.ly>
2025-04-08 13:15:17 -0500

    fix: pricing page and sections

--- app/(sites)/sites/[domain]/pricing/page.tsx
+++ app/(sites)/sites/[domain]/pricing/page.tsx
@@ -1,21 +1,10 @@
-import { ArrowLeft } from 'lucide-react'
-import Link from 'next/link'
 import { Pricing } from '@/components/sites/pages/pricing'
 import { withSitesWrapper } from '@/components/sites/with-sites-wrapper'
 
 async function PricingPage(props: { params: Promise<{ domain: string }> }) {
   const { domain } = await props.params
 
-  return (
-    <div className='container mx-auto max-w-5xl px-4 pt-24 pb-12 md:pt-32'>
-      <Link href='/' className='hover:text-primary mb-6 inline-flex items-center text-sm text-gray-500 transition-colors'>
-        <ArrowLeft className='mr-1 h-4 w-4' />
-        Back
-      </Link>
-
-      <Pricing />
-    </div>
-  )
+  return <Pricing />
 }
 
-export default withSitesWrapper(PricingPage)
+export default withSitesWrapper(PricingPage, false, false)
