c5e3e6dbc39e0999b095983cfb99c6bee9612a6e
Nathan Clevenger <nateclev@gmail.com>
2025-04-09 08:12:30 -0400

    docs: more content updates

--- content/_meta.ts
+++ content/_meta.ts
@@ -26,6 +26,7 @@ const meta: MetaRecord = {
   events: '',
   experiments: '',
   integrations: '',
+  deployment: '',
   observability: '',
   
 
--- content/deployment/_meta.ts
+++ content/deployment/_meta.ts
@@ -1,6 +1,7 @@
 import type { MetaRecord } from 'nextra'
 
 const meta: MetaRecord = {
+  code: '',
   types: '',
   modules: '',
   packages: '',
