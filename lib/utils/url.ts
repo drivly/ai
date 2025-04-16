7fd9084199cf80904756d77ff77799686a4018f1
Chris Risner <chris@driv.ly>
2025-04-16 17:31:58 -0500

    fix: spacing url.ts

--- lib/utils/url.ts
+++ lib/utils/url.ts
@@ -20,8 +20,7 @@ export const getCurrentURL = (headers?: Headers) => {
 
   // For development environment
   if (process.env.NODE_ENV === 'development') {
-    console.log('🚀 ~ localhost  URL: http://localhost:3000')
-    return 'http://localhost:3000'
+    console.log('🚀 ~ localhost URL: http://localhost:3000')
   }
 
   if (process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL) {
