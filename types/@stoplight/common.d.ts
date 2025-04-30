f9db3322feebac8205136ed98986d132c90c44ea
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-04-30 09:39:05 -0500

    chore: cleanup types

--- types/@stoplight/common.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@stoplight/markdown-viewer' {
-  const Component: any
-  export default Component
-}
-
-declare module '@stoplight/mosaic' {}
-
-declare module '@stoplight/mosaic-code-viewer' {}
