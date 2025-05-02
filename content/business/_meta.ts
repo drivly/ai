5348f491162a02afff79b4fd4374b7e07b355941
Nathan Clevenger <nateclev@gmail.com>
2025-05-02 11:02:58 -0500

    docs: hide kpis and projects

---
+++ content/business/_meta.ts
@@ -0,0 +1,9 @@
+import type { MetaRecord } from 'nextra'
+
+export default {
+  goals: '',
+  plans: '',
+  tasks: '',
+  kpis: { display: 'hidden' },
+  projects: { display: 'hidden' },
+} satisfies MetaRecord
