ff62eeba312b7edec99c6d7c5ecc4a51827288e1
AggressivelyMeows <25571984+AggressivelyMeows@users.noreply.github.com>
2025-05-02 18:29:35 +0000

    Add image routes from gpt-do's preview

---
+++ app/(apis)/llm/images/models/[modelName]/route.ts
@@ -0,0 +1,27 @@
+// Returns a model's author icon
+import { getModel } from '@/pkgs/language-models'
+import { NextRequest, NextResponse } from 'next/server'
+
+const getModelIcon = async (modelName: string): Promise<string> => {
+  const model = await getModel(modelName)
+
+  const modelAuthor = model.author
+
+  const html = await fetch(`https://openrouter.ai/${modelAuthor}`).then(res => res.text())
+
+  return 'https://openrouter.ai/images/icons/' + html
+    .split('/images/icons/')[1]
+    .split('\\"')[0]
+}
+
+export const GET = async (req: NextRequest, { params }: { params: Promise<Record<string, string | string[]>> }) => {
+  const { modelName } = await params
+
+  const icon = await fetch(await getModelIcon(modelName as string))
+
+  return new Response(icon.body, {
+    headers: {
+      'Content-Type': icon.headers.get('Content-Type') as string
+    }
+  })
+} 
