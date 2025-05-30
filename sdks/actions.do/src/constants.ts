7d5157d2cdf78552eea8c0cf1ff0158aa8fd0083
AggressivelyMeows <25571984+AggressivelyMeows@users.noreply.github.com>
2025-05-30 13:40:21 +0000

    chore: auto run

--- sdks/actions.do/src/constants.ts
+++ sdks/actions.do/src/constants.ts
@@ -1,2 +1,2 @@
-export const ACTION_NAMES = [] as const;
-export type ActionName = (typeof ACTION_NAMES)[number];
+export const ACTION_NAMES = [] as const
+export type ActionName = (typeof ACTION_NAMES)[number]
