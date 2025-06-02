df55edac541198ee6a37b057b729c238994f2fad
Samuel Lippert <lippertz@gmail.com>
2025-06-02 11:28:40 -0500

    build: types

--- tasks/data/createRecordTask.ts
+++ tasks/data/createRecordTask.ts
@@ -7,15 +7,15 @@ export const createRecordTask = {
     { name: 'collection', type: 'text', required: true },
     { name: 'data', type: 'json', required: true },
   ],
-  outputSchema: [{ name: 'record', type: 'json' }],
+  outputSchema: [{ name: 'record', type: 'json', required: true }],
   handler: async ({ input, req }) => {
     const { payload } = req
     const { data, collection } = input
-    const newRecord = await payload.create({
+    const record = await payload.create({
       collection: collection as CollectionSlug,
       data: data as Record<string, unknown>,
       req,
     })
-    return { output: { record: newRecord } }
+    return { output: { record: record as unknown as Record<string, unknown> } }
   },
 } as TaskConfig<'createRecord'>
