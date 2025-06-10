5921cfcff71d869eb6115d855e0658ef931128cb
Samuel Lippert <lippertz@gmail.com>
2025-06-10 11:10:13 -0500

    feat: add messages to prompt

--- collections/experiments/Prompts.ts
+++ collections/experiments/Prompts.ts
@@ -13,21 +13,79 @@ export const Prompts: CollectionConfig = {
     { name: 'system', type: 'text' },
     { name: 'modelName', type: 'text' },
     {
-      name: 'format',
-      type: 'group',
+      name: 'messages',
+      type: 'array',
       fields: [
-        { name: 'questions', type: 'array', fields: [{ name: 'question', type: 'text' }] },
-        { name: 'role', type: 'text' },
-        { name: 'instructions', type: 'array', fields: [{ name: 'instruction', type: 'text' }] },
-        { name: 'context', type: 'text' },
         {
-          name: 'examples',
+          name: 'role',
+          type: 'select',
+          options: ['system', 'user', 'assistant', 'tool'],
+          defaultValue: 'user',
+          required: true,
+        },
+        { name: 'contentString', type: 'text' },
+        {
+          name: 'contentArray',
           type: 'array',
           fields: [
-            { name: 'title', type: 'text' },
-            { name: 'example', type: 'text', required: true },
+            {
+              name: 'type',
+              type: 'select',
+              options: ['text', 'image', 'file', 'tool_call', 'tool_result', 'reasoning', 'redacted_reasoning'],
+            },
+            {
+              name: 'imageString',
+              type: 'text',
+            },
+            {
+              name: 'imageFile',
+              type: 'upload',
+              relationTo: 'files',
+            },
+            { name: 'dataString', type: 'text' },
+            { name: 'dataFile', type: 'upload', relationTo: 'files' },
+            { name: 'filename', type: 'text' },
+            { name: 'mimeType', type: 'text' },
+            {
+              name: 'text',
+              type: 'text',
+            },
+            { name: 'signature', type: 'text' },
+            { name: 'data', type: 'text' },
+            {
+              name: 'toolCallId',
+              type: 'text',
+            },
+            {
+              name: 'toolName',
+              type: 'text',
+            },
+            {
+              name: 'args',
+              type: 'json',
+            },
+            {
+              name: 'result',
+              type: 'json',
+            },
+            {
+              name: 'isError',
+              type: 'checkbox',
+            },
           ],
         },
+      ],
+    },
+    { name: 'questions', type: 'array', fields: [{ name: 'question', type: 'text' }] },
+    { name: 'role', type: 'text' },
+    { name: 'instructions', type: 'array', fields: [{ name: 'instruction', type: 'text' }] },
+    { name: 'context', type: 'text' },
+    {
+      name: 'examples',
+      type: 'array',
+      fields: [
+        { name: 'title', type: 'text' },
+        { name: 'example', type: 'text', required: true },
       ],
     },
     {
@@ -62,16 +120,14 @@ export const Prompts: CollectionConfig = {
         ],
         afterRead: [
           (({ data }) =>
-            (data?.format
+            (data
               ? `${data.name ? '# ' + data.name + '\n\n' : ''}${
-                  data.format.questions?.length ? '## Questions\n\n' + data.format.questions.map((q) => q.question).join('\n') + '\n\n' : ''
-                }${data.format.role ? '## Role\n\n' + data.format.role + '\n\n' : ''}${
-                  data.format.instructions?.length ? '## Instructions\n\n' + data.format.instructions.map((i) => i.instruction).join('\n') + '\n\n' : ''
-                }${data.format.context ? '## Context\n\n' + data.format.context + '\n\n' : ''}${
-                  data.format.examples?.length
-                    ? '## Examples\n\n' + data.format.examples.map((e) => `${e.title ? `### ${e.title}\n\n` : ''}${e.example}`).join('\n\n') + '\n\n'
-                    : ''
-                }${data.format.questions?.length ? '## Questions\n\n' + data.format.questions.map((q) => q.question).join('\n') + '\n' : ''}`
+                  data.questions?.length ? '## Questions\n\n' + data.questions.map((q) => q.question).join('\n') + '\n\n' : ''
+                }${data.role ? '## Role\n\n' + data.role + '\n\n' : ''}${
+                  data.instructions?.length ? '## Instructions\n\n' + data.instructions.map((i) => i.instruction).join('\n') + '\n\n' : ''
+                }${data.context ? '## Context\n\n' + data.context + '\n\n' : ''}${
+                  data.examples?.length ? '## Examples\n\n' + data.examples.map((e) => `${e.title ? `### ${e.title}\n\n` : ''}${e.example}`).join('\n\n') + '\n\n' : ''
+                }${data.questions?.length ? '## Questions\n\n' + data.questions.map((q) => q.question).join('\n') + '\n' : ''}`
               : '') + (data?.system || '')) as FieldHook<Prompt>,
         ],
       },
